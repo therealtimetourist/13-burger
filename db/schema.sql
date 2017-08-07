e243fcdcdac0898e7d66f97938543ed34749c3bf
Don Bedwell <timetourst@gmail.com>
2017-08-07 11:17:42 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,10 @@
-
+DROP TABLE IF EXISTS burgers_db;
+CREATE DATBASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers(
+  'id' INT NOT NULL AUTO_INCREMENT,
+  'burger_name' VARCHAR(25),
+  'devoured' BOOLEAN NOT NULL DEFAULT 0,
+  'date' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY('id')
+);
